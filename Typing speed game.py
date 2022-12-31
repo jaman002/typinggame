850a8d6f66f49a55963974f7741f3b0bbf2a989d
jaman002 <shajibblog@gmail.com>
2022-12-31 11:31:58 +0600

    updated

--- Typing speed game.py
+++ Typing speed game.py
@@ -1,3 +1,17 @@
-#print
-print("Hi")
+#import tkinter
+from tkinter import *
+
+root = Tk()
+
+root.title("Typing speed test game")
+root.geometry("1530x790+0+0")
+root.configure(bg="#dddddd")
+root.iconbitmap()
+
+
+
+
+
+root.mainloop()
+
 
